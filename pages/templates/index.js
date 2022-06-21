d96fa23c353259f78872b2e0d9e1ac5583b0aebb
PasztorGergo <pasztorg05@gmail.com>
2022-06-21 20:07:57 +0200

    Apostrophe fix

--- pages/templates/index.js
+++ pages/templates/index.js
@@ -11,8 +11,10 @@ export default function Templates() {
       <header className="flex flex-col justify-evenly items-center px-5 py-3 bg-slate-800 text-white min-h-fit mt-56 m-auto gap-y-4 w-2/3 rounded-lg">
         <h1 className="text-3xl font-bold">So curious! 👀</h1>
         <h2 className="text-xl font-semibold">
-          It's great to see your would like to explore pre-built forms. <br />
-          Sadly, I'm not done yet but it's worth to keep an eye on this page!
+          It&apos;s great to see your would like to explore pre-built forms.{" "}
+          <br />
+          Sadly, I&apos;m not done yet but it&apos;s worth to keep an eye on
+          this page!
           <br />
           Thank you, and see you later!
         </h2>
