8c713087b478275553d7dad45946b8bdd1932841
PasztorGergo <pasztorg05@gmail.com>
2022-06-22 20:27:00 +0200

    meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
 import { Footer } from "../components";
 import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta name="title" content="Formified | Professional Form editor" />
+        <meta
+          name="description"
+          content="Create beautiful forms for your web pages with Formified! Add inputs, selections, headings, and many more."
+        />
+        <meta name="og:title" content="Formified | Professional Form editor" />
+        <meta
+          name="og:description"
+          content="Create beautiful forms for your web pages with Formified! Add inputs, selections, headings, and many more."
+        />
+      </Head>
       <Component {...pageProps} />
       <Footer />
     </>
